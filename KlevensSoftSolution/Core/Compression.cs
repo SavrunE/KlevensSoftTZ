8e7e679a0bd57c0a6974afc9f64028877b96908b
SavrunE <69328877+SavrunE@users.noreply.github.com>
2026-01-21 06:43:54 +0300

    compression line rdy

---
+++ KlevensSoftSolution/Core/Compression.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+
+public class Compression
+{
+    public string Compress(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return input;
+        StringBuilder compressed = new StringBuilder();
+
+        char currentChar = input[0];
+        int count = 1;
+
+        for (int i = 1; i < input.Length; i++)
+        {
+            if (input[i] == currentChar)
+            {
+                count++;
+            }
+            else
+            {
+                compressed.Append(currentChar);
+                if (count > 1)
+                {
+                    compressed.Append(count);
+                }
+                currentChar = input[i];
+                count = 1;
+            }
+        }
+
+        compressed.Append(currentChar);
+        if (count > 1)
+        {
+            compressed.Append(count);
+        }
+
+        return compressed.ToString();
+    }
+
+    public string Decompress(string input)
+    {
+        StringBuilder decompressed = new StringBuilder();
+        char currentChar = '0';
+        string countString = string.Empty;
+
+        foreach (char c in input)
+        {
+            if (char.IsLetter(c))
+            {
+                if (currentChar != '0')
+                {
+                    int count = string.IsNullOrEmpty(countString) ? 1 : int.Parse(countString);
+                    decompressed.Append(new string(currentChar, count));
+                }
+                currentChar = c;
+                countString = string.Empty;
+            }
+            else if (char.IsDigit(c))
+            {
+                countString += c;
+            }
+        }
+
+        if (currentChar != '0')
+        {
+            int count = string.IsNullOrEmpty(countString) ? 1 : int.Parse(countString);
+            decompressed.Append(new string(currentChar, count));
+        }
+
+        return decompressed.ToString();
+    }
+}
+
---
+++ KlevensSoftSolution/Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ItShould_string_compare_correctly()
+        {
+            // Arrange
+            string input = "aaabbcccdde";
+            string expectedCompressed = "a3b2c3d2e";
+            var sut = new Compression();
+            var actualCompressed = sut.Compress(input);
+            // Act
+            // Assert
+            Assert.Equal(expectedCompressed, actualCompressed);
+        }
+    }
+}
